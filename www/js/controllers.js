63153a19e6b09664cce1947177d5a78217d4075f
Patricio Córdova <patricio@cs.toronto.edu>
2015-07-27 23:09:09 -0400

    added feed and login controllers

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -58,4 +58,36 @@ angular.module('starter.controllers', [])
 .controller('HomeCtrl', function($scope) {
   $scope.helloWorld = "Hello human";
 })
+
+.controller("LoginController", function($scope, $cordovaOauth, $localStorage, $location) {
+ 
+    $scope.login = function() {
+        $cordovaOauth.facebook("andrscordova", ["email", "read_stream", "user_website", "user_location", "user_relationships"]).then(function(result) {
+            $localStorage.accessToken = result.access_token;
+            $location.path("/profile");
+        }, function(error) {
+            alert("There was a problem signing in!  See the console for logs");
+            console.log(error);
+        });
+    };
+ 
+})
+
+.controller("ProfileController", function($scope, $http, $localStorage, $location) {
+ 
+    $scope.init = function() {
+        if($localStorage.hasOwnProperty("accessToken") === true) {
+            $http.get("https://graph.facebook.com/v2.2/me", { params: { access_token: $localStorage.accessToken, fields: "id,name,gender,location,website,picture,relationship_status", format: "json" }}).then(function(result) {
+                $scope.profileData = result.data;
+            }, function(error) {
+                alert("There was a problem getting your profile.  Check the logs for details.");
+                console.log(error);
+            });
+        } else {
+            alert("Not signed in");
+            $location.path("/login");
+        }
+    };
+ 
+})
 ;
