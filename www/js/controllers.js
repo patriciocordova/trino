029b4d33ba47bf59b59e813ed701c84d686773cb
Patricio Córdova <patricio@cs.toronto.edu>
2015-07-29 23:57:05 -0400

    feed controller refactored to print errors to console

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -80,7 +80,7 @@ angular.module('starter.controllers', [])
             $http.get("https://graph.facebook.com/v2.2/me", { params: { access_token: $localStorage.accessToken, fields: "id,name,gender,location,website,picture,relationship_status", format: "json" }}).then(function(result) {
                 $scope.profileData = result.data;
             }, function(error) {
-                alert("There was a problem getting your profile.  Check the logs for details.");
+                alert("There was a problem getting your profile. Check the logs for details.");
                 console.log(error);
             });
         } else {
