5d0e958b745e18b36af41ef7331d6a374100d000
Patricio Córdova <patricio@cs.toronto.edu>
2015-08-02 23:29:35 -0400

    removed feed controller since we won't show user tweets

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -90,25 +90,6 @@ angular.module('starter.controllers', [])
     };
  
 })
-
-.controller("FeedController", function($scope, $http, $localStorage, $location) {
- 
-    $scope.init = function() {
-        if($localStorage.hasOwnProperty("accessToken") === true) {
-            $http.get("https://graph.facebook.com/v2.2/me/feed", { params: { access_token: $localStorage.accessToken, format: "json" }}).then(function(result) {
-                $scope.feedData = result.data.data;
-                $http.get("https://graph.facebook.com/v2.2/me", { params: { access_token: $localStorage.accessToken, fields: "picture", format: "json" }}).then(function(result) {
-                    $scope.feedData.myPicture = result.data.picture.data.url;
-                });
-            }, function(error) {
-                alert("There was a problem getting your profile. Check the logs for details.");
-                console.log(error);
-            });
-        } else {
-            alert("Not signed in");
-            $location.path("/login");
-        }
-    };
  
 })
 ;
